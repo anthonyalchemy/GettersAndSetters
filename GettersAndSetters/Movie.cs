2d6870c333a1037879fb400fc444dbdb9c2fea7e
anthonyalchemy <anthony@alchemytechs.com>
2019-07-31 15:37:59 +0100

    Add project files.

---
+++ GettersAndSetters/Movie.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GettersAndSetters
+{
+    class Movie
+    {
+        public string title;
+        public string director;
+        private string rating; // only code inside movie class can access this due to private
+
+        public Movie (string aTitle, string aDirector, string aRating) // contructor 
+        {
+            title = aTitle;
+            director = aDirector;
+            Rating = aRating;
+        }
+
+        public string Rating
+        {
+            get { return rating; } // getter allows us to get rating attribute
+            set {
+                if (value == "G" || value == "PG" || value == "PG-13" || value == "R" || value == "NR") 
+                {
+                    rating = value;
+                }
+                else
+                {
+                    rating = "NR"; // set to not rating if not entered 
+                }
+            } //setter allows us to set the rating attribute 
+        }
+
+
+    }
+}
---
+++ GettersAndSetters/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace GettersAndSetters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Movie avengers= new Movie("The Avengers", "Josh Whedon", "PG-13");
+            Movie shrek = new Movie("Shrek", "Adam Adamson", "PG");
+            //G, PG, PG-13, R, NR 
+
+            Console.WriteLine(avengers.Rating);
+            Console.WriteLine(shrek.Rating);
+
+            Console.ReadLine();
+
+        }
+    }
+}
